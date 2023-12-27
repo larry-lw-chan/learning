1d276d84648d8e2a1029a5a089d4a0ef4eedc3b0
Larry Chan <larrychan@Larrys-MacBook-Pro.local>
2023-12-27 13:44:58 -0500

    dotnet stuff

--- Csharp/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ Csharp/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62efa43ebeb450f73586b99d9c250d3a442d5cd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+816235e05e24d9b4b319785dafefc8b0cecc8680")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
